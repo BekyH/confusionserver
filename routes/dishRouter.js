67c28b05a1aceabd6ffb09d4434c5cae3b4a1271
unknown <bereketheramo25@gmail.com>
2020-05-27 14:28:33 +1200

    modification in http methods

--- routes/dishRouter.js
+++ routes/dishRouter.js
@@ -1,40 +1,74 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const Dishes = require('../models/dishes');
+
 const dishRouter = express.Router();
 dishRouter.use(bodyParser.json());
 dishRouter.route('/')
-.all((req,res,next)=>{
-    res.statusCode = 200;
-    res.setHeader('Content-Type','text/plain');
-    next();
-})
 
 .get((req,res,next)=>{
-    res.end("will send all dishes to you");
+    Dishes.find({})
+    .then((dishes)=>{
+        res.statusCode = 200;
+        res.setHeader('Content-Type','application/json');
+        res.json(dishes);
+    },(err)=>{
+        next(err);
+    }).catch((err)=>{
+        next(err);
+    })
+
 })
 .post((req,res,next)=>{
-    res.end('will add the dish: ' + req.body.name);
-
+    Dishes.create(req.body)
+    .then((dish)=>{
+        console.log("Dish created ",dish);
+        res.statusCode = 200;
+        res.setHeader('Content-Type','application/json');
+        res.json(dish);
+    },(err)=>{
+        next(err);
+    }).catch((err)=>{
+        next(err);
+    })
 })
 .put((req,res,next)=>{
     res.statusCode = 403;
     res.end('Put operation not supported');
 })
 .delete((req,res,next)=>{
-    res.end("deleting all the dishes");
+    Dishes.remove({})
+    .then((rsp)=>{
+        res.statusCode = 200;
+        res.setHeader('Content-type','application/json');
+        res.json(resp);
+    },(err)=>{
+        next(err);
+    }).catch((err)=>{
+        next(err);
+    })
 });
 dishRouter.route('/:dishId')
 .get((req,res,next)=>{
-    res.end("will send the dish: " + req.params.dishId);
+    Dishes.findById(req.params.dishId)
+    .then((dish)=>{
+        res.statusCode = 200;
+        res.setHeader('Content-Type','application/json');
+        res.json(dish);
+
+    },(err)=>next(err))
+    .catch((err)=>next(err));
 
 })
 .post((req,res,next)=>{
+    res.statusCode = 403;
     res.end("403 post operation is not supported on the dish: " + req.params.dishId);
 })
 .put((req,res,next)=>{
-    res.end("will update the disha: " + req.params.dishId);
+    
 })
 .delete((req,res,next)=>{
-    res.end("delete the dish: " + req.params.dishId);
+    
 })
 module.exports = dishRouter;
