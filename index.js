db3fd4b428c9d3624efba90ff9dd50dc26667da1
unknown <bereketheramo25@gmail.com>
2020-05-30 22:02:54 +1200

    change in index

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ var session = require('express-session');
 var FileStore = require('session-file-store')(session);
 var passport = require('passport');
 var authenticate = require('./authenticate');
+var config = require('./config');
 const userRouter = require('./routes/userRouter');
 const dishRouter = require('./routes/dishRouter');
 const promoRouter = require('./routes/promoRouter');
@@ -20,7 +21,7 @@ const leaderRouter = require('./routes/leaderRouter');
 const hostname = 'localhost';
 const port = 3000;
 
-const url = 'mongodb://localhost:27017/conFusion';
+const url = config.mongoUrl;
 const connect = mongoose.connect(url);
 connect.then((db)=>{
     console.log("correctly connected to the mongodb server");
@@ -33,16 +34,9 @@ const app = express();
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 //app.use(cookieParser('12345-67890-09876-54321'));
-app.use(session({
-    name:'session-id',
-    secret:'12345-67890-09876-54321',
-    saveUninitialized:false,
-    resave:false,
-    store:new FileStore()
 
-}));
 app.use(passport.initialize());
-app.use(passport.session());
+
 app.use((req,res,next)=>{
     console.log(req.headers);
     res.statusCode = 200;
@@ -50,28 +44,7 @@ app.use((req,res,next)=>{
      res.end('<html><body><h1>This is an express server</h1></body></html>');
 });
 app.use('/users',userRouter);
-function auth(req,res,next){
 
-    if(!req.user){
-        
-        
-        res.statusCode = 403;
-        res.setHeader('WWW-authenticate','Basic');
-        res.end("you are not authenticated");
-}
-    else{
-        
-            next();
-
-        }
-         
-    }
-
-    
-
-
-
-app.use(auth);
 
 app.use('/dishes',dishRouter);
 app.use('/promotions',promoRouter);
