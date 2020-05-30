4dd50f77858cea500dd444a7baab863f1b800b5b
unknown <bereketheramo25@gmail.com>
2020-05-30 21:28:20 +1200

    little changes

--- authenticate.js
+++ authenticate.js
@@ -8,3 +8,30 @@ var config = require('./config');
 exports.local = passport.use(new LocalStrategy(user.authenticate()));
 passport.serializeUser(user.serializeUser()); // determines which data of the user object should be stored in the session
 passport.deserializeUser(user.deserializeUser());
+
+exports.getToken = function(user){
+    return jwt.sign(user,config.secretKey,{expiresIn:3600});
+
+};
+var opts = {};
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = config.secretKey;
+
+exports.jwtPassport = passport.use(new JwtStrategy(opts,(jwt_payload,done)=>{
+    console.log("JWT payload: ",jwt_payload);
+    user.findOne({_id:jwt_payload._id},(err,user)=>{
+        if(err){
+            return done(err,false);
+
+        }
+        else if(user){
+            return done(null,user);
+        }
+        else{
+            return done(err,false);
+        }
+    });
+
+}));
+
+exports.verifyUser = passport.authenticate('jwt',{session:false});
