ed0f00e69f573fc41163e2abf6f650d265657f63
unknown <bereketheramo25@gmail.com>
2020-05-30 20:43:46 +1200

    modification

--- authenticate.js
+++ authenticate.js
@@ -1,7 +1,10 @@
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 var user = require('./models/users');
-
+var JwtStrategy = require('passport-jwt').Strategy;
+var extractJwt = require('passport-jwt').ExtractJwt;
+var jwt = require('jsonwebtoken');
+var config = require('./config');
 exports.local = passport.use(new LocalStrategy(user.authenticate()));
 passport.serializeUser(user.serializeUser()); // determines which data of the user object should be stored in the session
 passport.deserializeUser(user.deserializeUser());
