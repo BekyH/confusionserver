81e39c6f33d8a6a724522907accd1ea8daf218b5
unknown <bereketheramo25@gmail.com>
2020-06-05 17:25:47 +1200

    changes

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ module.exports = {
     'secretKey': '12345-67890-09876-54321',
     'mongoUrl' : 'mongodb://localhost:27017/conFusion',
     'facebook':{
-        clienId:'271780310901271',
-        clientSecret:'28695b0558b7414fb6ddcc033306feea'
+        clienId:clienId,
+        clientSecret:clientSecret
     }
 
 }
--- index.js
+++ index.js
@@ -47,13 +47,15 @@ app.use('/promotions',promoRouter);
 app.use('/leaders',leaderRouter);
 app.use('/imageupload',uploadRouter);
 
-app.use('/',(req,res,next)=>{
-    console.log(req.headers);
-    res.statusCode = 200;
-    res.setHeader('Content-Type','text/html');
-     res.end('<html><body><h1>This is an express server</h1></body></html>');
+// app.use('/',(req,res,next)=>{
+//     console.log(req.headers);
+//     res.statusCode = 200;
+//     res.setHeader('Content-Type','text/html');
+//      res.end('<html><body><h1>This is an express server</h1></body></html>');
+// });
+app.use('/index',(req,res,next)=>{
+    res.sendFile(path.join(__dirname+'/public/index.html'));
 });
-
 
 const server = http.createServer(app);
 server.listen(port,hostname,()=>{
